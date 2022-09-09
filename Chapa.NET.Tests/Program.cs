c9ecbaa5b11342b9a7483e93f5f7b953b1a28cd6
kerodkibatu <kerod5858@outlook.com>
2022-09-10 02:05:12 +0300

    documented actions

--- Chapa.NET.Tests/Program.cs
+++ Chapa.NET.Tests/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//Import Chapa.NET
+using ChapaNET;
+
+//Initialize your Chapa Instance
+Chapa chapa = new Chapa("CHASECK-*YOUR-API-KEY*");
+
+//Get a unique transaction ID
+var ID = Guid.NewGuid().ToString();
+
+//Make a request
+var Request = new ChapaRequest(amount: 30.52, email: "abebebikila@gmail.com", first_name: "Abebe", last_name: "Bikila", tx_ref: ID);
+
+//Process the request and get a response asynchronously
+var Result = await chapa.Request(Request);
+
+//Print out the message
+Console.WriteLine(Result.Message);
+
+//Wait For 1min
+Thread.Sleep(TimeSpan.FromMinutes(1));
+
+//Verify Transaction
+Console.WriteLine(chapa.Verify(ID));

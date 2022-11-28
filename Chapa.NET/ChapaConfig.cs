ffa84cd35fe8d18d3ae0f94523cc96346f9be3e1
kerodkibatu <kerod5858@outlook.com>
2022-11-28 20:09:43 +0300

    restored config file

---
+++ Chapa.NET/ChapaConfig.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ChapaNET;
+public class ChapaConfig
+{
+    public const string AUTH_HEADER = "Authorization";
+    public const string ACCEPTS_ENCODING_HEADER = "acceptEncodingHeader";
+
+    public string API_SECRET { get; set; } = string.Empty;
+    public const string BASE_PATH = "https://api.chapa.co/v1";
+
+}
